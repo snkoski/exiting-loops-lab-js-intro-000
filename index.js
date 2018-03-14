4ca670c24e8df19a7684d17db83ceb7047a3c696
Shawn Koski <shawnnkoski@gmail.com>
2018-03-14 23:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function findBy(array, findFn) {
   
   
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === findFn(array)) {
+    if (array[i] === findFn(array[i])) {
       return array[i];
     }
   }
